b607d680c1502b82c5bfcb09bcd545c5e56aff33
Anghel Ionut <mangalagiuanghel@gmail.com>
2025-07-23 20:40:26 +0200

    navBar&Hero
    He añadido la barra de navegación y el Hero con una transición

---
+++ src/app/components/nav-bar/nav-bar.ts
@@ -0,0 +1,19 @@
+import { Component,  HostListener } from '@angular/core';
+
+@Component({
+  selector: 'nav-bar',
+  imports: [],
+  templateUrl: './nav-bar.html',
+  styleUrl: './nav-bar.css'
+})
+export class NavBar {
+
+  // BARRA DE NAVEGACION
+  isFixed: boolean = false;
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isFixed = window.scrollY > 50;}
+
+
+    
+}
