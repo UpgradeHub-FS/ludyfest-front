bf3a3532b372585a4d9686ed11a91d835b0b6497
Maria Patricia Ruiz de Pedraza <patricia.ruiz6248@gmail.com>
2025-07-24 18:03:15 +0200

    Añadida la ruta para mostrar el listado de eventos privado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { PublicEventListComponent } from './pages/public-event-list/public-event-list.component';
+import { PrivateEventList } from './pages/private-event-list/private-event-list';
+
+
 
 export const routes: Routes = [
-    {path: 'home', component: Home},
-    {path: 'events', component: PublicEventListComponent}
+    { path: 'home', component: Home },
+    { path: 'events', component: PublicEventListComponent },
+    { path: 'events/admin', component: PrivateEventList },
+    { path: '**', redirectTo: 'home' } //si no encuentra la pag que vaya al home
 ];
+
+
+
