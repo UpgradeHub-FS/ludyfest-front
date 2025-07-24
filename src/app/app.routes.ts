281b3b9faa997a6db26f72b802366aa6a614ace8
Alejandro Valdés <alejandrovaldesp86@gmail.com>
2025-07-24 17:06:33 +0200

    Ruta del login incorporada en el archivo app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { PublicEventListComponent } from './pages/public-event-list/public-event-list.component';
+import { Login } from './pages/login/login';
 
 export const routes: Routes = [
-    {path: 'home', component: Home},
-    {path: 'events', component: PublicEventListComponent}
+    { path: 'home', component: Home },
+    { path: 'events', component: PublicEventListComponent },
+    { path: 'login', component: Login }
+
 ];
