beba2116940d6af9136cfe12b79ad80ede175038
Maria Patricia Ruiz de Pedraza <patricia.ruiz6248@gmail.com>
2025-07-24 18:50:35 +0200

    Creada file event.service junto con el getall

---
+++ src/app/services/event.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { IEvent } from '../interfaces/IEvents';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventService {
+
+  private httpClient = inject(HttpClient);
+  private baseUrl: string = 'http://localhost:8000/events';
+
+  getAll() {
+    return lastValueFrom(
+      this.httpClient.get<IEvent[]>(`${this.baseUrl}/admin`)
+    )
+  }
+
+}
