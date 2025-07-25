6ff49d3525f5bf4b2f2ac96584be7ad8e74d1cf9
GuiMayor <villawolflobo@gmail.com>
2025-07-25 11:51:29 +0200

    vista del events componente y ruta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
+
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { EventsComponent } from './pages/events/events.component';
 
 export const routes: Routes = [
-    {path: 'home', component: Home}
+  { path: 'home', component: Home },
+  { path: 'events/:id', component: EventsComponent }
 ];
