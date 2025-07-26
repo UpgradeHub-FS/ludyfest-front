9fd301863cd4a76c115e47eb24485f0ff229ff8d
GuiMayor <villawolflobo@gmail.com>
2025-07-26 12:04:06 +0200

    finalizado perfil UsuarioPrivado

--- src/app/pages/events/events.component.ts
+++ src/app/pages/events/events.component.ts
@@ -3,11 +3,12 @@ import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-events',
-  templateUrl: './events.html',
-  styleUrls: ['./events.css']
+  templateUrl: './events.component.html',
+  styleUrls: ['./events.component.css']
 })
 export class EventsComponent {
   id: string | null = null;
+user: any;
 
   constructor(private route: ActivatedRoute) {
     this.route.paramMap.subscribe(params => {
