bac0f9c87ec129083bd7a8596234ddf136e21b2b
Anghel Ionut <mangalagiuanghel@gmail.com>
2025-07-28 17:37:02 +0200

    rutas para los botones y las cards

--- src/app/components/card-event/card-event.ts
+++ src/app/components/card-event/card-event.ts
@@ -1,19 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { IEvent } from '../../interfaces/IEvents';
-
+import { RouterLink} from '@angular/router';
 @Component({
   selector: 'card-event',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './card-event.html',
   styleUrl: './card-event.css'
 })
 
 export class CardEvent {
-  // EVENT PROVISONAL
+
   @Input() event: IEvent | undefined;
-
-
-
 
 
 }
