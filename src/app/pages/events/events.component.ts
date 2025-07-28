eda839586564df0c547789d8231317f2c20cf497
GuiMayor <villawolflobo@gmail.com>
2025-07-28 12:09:37 +0200

    CSS HTML y JS corregido de Events y private-user

--- src/app/pages/events/events.component.ts
+++ src/app/pages/events/events.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { EventService } from './../../services/event.service';
 import { IEvent } from './../../interfaces/IEvents';
+import { Observable, from } from 'rxjs';
 
 @Component({
   selector: 'app-events',
@@ -12,30 +13,24 @@ import { IEvent } from './../../interfaces/IEvents';
   styleUrls: ['./events.component.css']  
 })
 export class EventsComponent {
-  event: IEvent | undefined;
   private eventService = inject(EventService);
   private route = inject(ActivatedRoute);
 
-  async ngOnInit() {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
-    if (!isNaN(id)) {
-      const response = await this.eventService.getEventById(id);
-      this.event = response;
-    }
-  }
+  // promesa en observable
+  public event$: Observable<IEvent | undefined> = from(
+    this.eventService.getEventById(Number(this.route.snapshot.paramMap.get('id')))
+  );
 
-  // transforma el número de estado a un texxttoo
   getStatusText(status: number): string {
-    switch(status) {
+    switch (status) {
       case 1: return 'Upcoming';
       case 2: return 'Cancelled';
       default: return 'Unknown';
     }
   }
 
-  // Devuelve la classe CSS al estado
   getStatusClass(status: number): string {
-    switch(status) {
+    switch (status) {
       case 1: return 'upcoming';
       case 2: return 'cancelled';
       default: return '';
