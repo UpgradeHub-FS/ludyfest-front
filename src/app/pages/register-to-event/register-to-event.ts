dd9b1e399dfc8d1c7f972de25bbe88374e80d10c
Maria Patricia Ruiz de Pedraza <patricia.ruiz6248@gmail.com>
2025-07-28 23:43:32 +0200

    Creados componentes formulario register to events y ruta

---
+++ src/app/pages/register-to-event/register-to-event.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-to-event',
+  imports: [],
+  templateUrl: './register-to-event.html',
+  styleUrl: './register-to-event.css'
+})
+export class RegisterToEvent {
+
+}
