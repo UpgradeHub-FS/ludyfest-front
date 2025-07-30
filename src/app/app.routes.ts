fc13dbf4d8c7d9ea018a9cbb06f1a8c2e688dc9a
Jonatan <jgutierreziglesias@gmail.com>
2025-07-30 15:59:46 +0200

    guards funcionales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 
-import { Routes } from '@angular/router';
+import { Routes, Router, CanActivate } from '@angular/router';
 import { Home } from './pages/home/home';
 import { PublicEventListComponent } from './pages/public-event-list/public-event-list.component';
 import { PrivateEventList } from './pages/private-event-list/private-event-list';
@@ -11,21 +11,24 @@ import { PrivateUserList } from './pages/private-user-list/private-user-list';
 import { PrivateEventEdit } from './pages/private-event-edit/private-event-edit';
 import { PrivateEventPost } from './pages/private-event-post/private-event-post';
 import { UpdateUser } from './pages/update-user/update-user';
+import { adminGuard, authGuard } from './guards/guards';
+
 
 
 export const routes: Routes = [
     { path: 'home', component: Home },
     { path: 'events', component: PublicEventListComponent },
     { path: 'events/:idEvent', component: EventsComponent }, //OJO he cambiado 'id' a 'idEvent' !!!!!
-    { path: 'events/private/admin', component: PrivateEventList },
-    { path: 'events/private/admin/put/:idEvent', component: PrivateEventEdit },
-    { path: 'events/private/admin/post', component: PrivateEventPost },
-    { path: 'users/private/admin', component: PrivateUserList },
+    { path: 'events/private/admin', component: PrivateEventList, canActivate: [adminGuard] },
+    { path: 'events/private/admin/put/:idEvent', component: PrivateEventEdit, canActivate: [adminGuard] },
+    { path: 'events/private/admin/post', component: PrivateEventPost, canActivate: [adminGuard] },
+    { path: 'users/private/admin', component: PrivateUserList, canActivate: [adminGuard] },
     { path: 'login', component: Login },
     { path: 'register', component: Register },
-    { path: 'profile', component: PrivateUserComponent },
-    { path: 'update-user', component: UpdateUser },
+    { path: 'profile', component: PrivateUserComponent, canActivate: [authGuard] },
+    { path: 'update-user', component: UpdateUser, canActivate: [authGuard] },
     { path: '**', redirectTo: 'home' } //si no encuentra la pag que vaya al home
+    
 ];
 
 
