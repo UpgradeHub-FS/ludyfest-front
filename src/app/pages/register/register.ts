56706f27637a186b018ac39f4f2cba46855c7cd5
Alejandro Valdés <alejandrovaldesp86@gmail.com>
2025-07-30 23:44:40 +0200

    Modificación en las validaciones del register, contraseña y repite contraseña

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -12,8 +12,8 @@ import Swal from 'sweetalert2';
 })
 export class Register {
 
-  UserService = inject (UserService)
-  router = inject (Router);
+  UserService = inject(UserService)
+  router = inject(Router);
 
   formulario: FormGroup = new FormGroup({
     name: new FormControl(null, [
@@ -24,46 +24,46 @@ export class Register {
       Validators.required,
       Validators.pattern(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)
     ]),
-    password: new FormControl(null,[Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).{5,10}$/)]),
-    repite_password: new FormControl(),
+    password: new FormControl(null, [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).{5,10}$/)]),
+    repite_password: new FormControl(null, [Validators.required]),
     rol: new FormControl(),
   }, [this.passwordValidator]);
-   checkError(field: string, error: string) {
+  checkError(field: string, error: string) {
     return this.formulario.get(field)?.hasError(error) &&
       this.formulario.get(field)?.touched;
   }
 
-  async onSubmit(){
-    try{
+  async onSubmit() {
+    try {
       const response = await this.UserService.registro(this.formulario.value);
       await Swal.fire({
         title: 'Exito',
-        text:'Usuario creado correctamente',
-        icon:'success'
+        text: 'Usuario creado correctamente',
+        icon: 'success'
       });
       this.router.navigateByUrl('/home')
 
 
-  } catch (error) {
-      
+    } catch (error) {
+
       Swal.fire({
         title: 'Error',
-        text:'Error en el envio. Revisa el formulario',
-        icon:'error'
+        text: 'Error en el envio. Revisa el formulario',
+        icon: 'error'
       });
     }
-    
+
   }
 
-   passwordValidator(form: AbstractControl){
+  passwordValidator(form: AbstractControl) {
     //extraer los valores de password y repite_password
     const passwordValue = form.get('password')?.value;
     const repitePasswordValue = form.get('repite_password')?.value;
 
-    if(passwordValue !== repitePasswordValue){
-      form.get('repite_password')?.setErrors({passwordValidator: true});
+    if (passwordValue !== repitePasswordValue) {
+      form.get('repite_password')?.setErrors({ passwordValidator: true });
 
-      return { passwordvalidator: true};
+      return { passwordvalidator: true };
     }
 
     return null;
