53b50e64103a789cc24cc036500b06c387de9cf2
Alejandro Valdés <alejandrovaldesp86@gmail.com>
2025-07-31 14:39:47 +0200

    Tarea terminada

--- src/app/pages/private-event-post/private-event-post.ts
+++ src/app/pages/private-event-post/private-event-post.ts
@@ -16,36 +16,38 @@ export class PrivateEventPost {
   router = inject(Router)
 
   imagenesDisponibles: string[] = [
-    'assets/img/ajedrez.jpg',
-    'assets/img/arte.jpg',
-    'assets/img/basket.jpg',
-    'assets/img/cine.jpg',
-    'assets/img/clasico.jpg',
-    'assets/img/cocina.jpg',
-    'assets/img/djparty.jpg',
-    'assets/img/docu.jpg',
-    'assets/img/fifa.jpg',
-    'assets/img/flamenco.jpg',
-    'assets/img/foodtrucks.jpg',
-    'assets/img/foto.jpg',
-    'assets/img/halloween.jpg',
-    'assets/img/indie.jpg',
-    'assets/img/infantil.jpg',
-    'assets/img/jazzfest.jpg',
-    'assets/img/kayak.jpg',
-    'assets/img/padel.jpg',
-    'assets/img/pintura.jpg',
-    'assets/img/playa.jpg',
-    'assets/img/reggateon.jpg',
-    'assets/img/rock.jpg',
-    'assets/img/senderismo.jpg',
-    'assets/img/teatro.jpg',
-    'assets/img/terror.jpg'
+    'img/ajedrez.jpg',
+    'img/arte.jpg',
+    'img/basket.jpg',
+    'img/cine.jpg',
+    'img/clasico.jpg',
+    'img/cocina.jpg',
+    'img/djparty.jpg',
+    'img/docu.jpg',
+    'img/fifa.jpg',
+    'img/flamenco.jpg',
+    'img/foodtrucks.jpg',
+    'img/foto.jpg',
+    'img/halloween.jpg',
+    'img/indie.jpg',
+    'img/infantil.jpg',
+    'img/jazzfest.jpg',
+    'img/kayak.jpg',
+    'img/padel.jpg',
+    'img/pintura.jpg',
+    'img/playa.jpg',
+    'img/reggateon.jpg',
+    'img/rock.jpg',
+    'img/senderismo.jpg',
+    'img/teatro.jpg',
+    'img/terror.jpg'
   ];
 
 
+
+
   formulario: FormGroup = new FormGroup({
-    
+
     title: new FormControl(null, [
       Validators.required,
 
@@ -90,52 +92,52 @@ export class PrivateEventPost {
       Validators.required,
 
     ]),
-    
+
   });
 
   async onSubmit() {
-  try {
-    const raw = this.formulario.value;
+    try {
+      const raw = this.formulario.value;
 
-    const payload = {
-      ...raw,
-      price: parseFloat(raw.price),
-      capacity: parseInt(raw.capacity, 10),
-      latitude: parseFloat(raw.latitude),
-      longitude: parseFloat(raw.longitude),
-      status: parseInt(raw.status, 10),
-      categories_id: parseInt(raw.categories_id, 10),
-      // 👇 Aquí transformamos la fecha al formato ISO completo
-      date: new Date(raw.date).toISOString().split('.')[0], // Esto elimina ".000Z"
-    };
+      const payload = {
+        ...raw,
+        price: parseFloat(raw.price),
+        capacity: parseInt(raw.capacity, 10),
+        latitude: parseFloat(raw.latitude),
+        longitude: parseFloat(raw.longitude),
+        status: parseInt(raw.status, 10),
+        categories_id: parseInt(raw.categories_id, 10),
+        // 👇 Aquí transformamos la fecha al formato ISO completo
+        date: new Date(raw.date).toISOString().split('.')[0], // Esto elimina ".000Z"
+      };
 
-    console.log('Payload a enviar:', payload);
+      console.log('Payload a enviar:', payload);
 
-    const response = await this.eventService.createEvent(payload);
-    console.log(response);
+      const response = await this.eventService.createEvent(payload);
+      console.log(response);
 
-    await Swal.fire({
-      title: 'Éxito',
-      text: '¡Se ha creado un nuevo evento!',
-      icon: 'success'
-    });
+      await Swal.fire({
+        title: 'Éxito',
+        text: '¡Se ha creado un nuevo evento!',
+        icon: 'success'
+      });
 
-    this.router.navigateByUrl('/events/private/admin');
+      this.router.navigateByUrl('/events/private/admin');
 
-  } catch (error: any) {
-    console.log('Error en el submit:', error);
+    } catch (error: any) {
+      console.log('Error en el submit:', error);
 
-    let message = 'Error en el envío, revisa el formulario.';
-    if (error.error && error.error.detail) {
-      message = JSON.stringify(error.error.detail);
+      let message = 'Error en el envío, revisa el formulario.';
+      if (error.error && error.error.detail) {
+        message = JSON.stringify(error.error.detail);
+      }
+
+      Swal.fire({
+        title: 'Error',
+        text: message,
+        icon: 'error'
+      });
     }
-
-    Swal.fire({
-      title: 'Error',
-      text: message,
-      icon: 'error'
-    });
   }
-}
 
 }
